57f9b6725ef258d96fe0d4083ab9510f1744e29f
User01 <rigbykai1@gmail.com>
2023-09-05 16:04:28 -0600

    Backend addition

---
+++ NotasBackEnd/Notes/Notes.js
@@ -0,0 +1,71 @@
+export let notes = [
+  {
+    "id": "df1e4975-3521-4b78-ad01-1af0bc44920a",
+    "title": "HTML",
+    "content": "HTML is easy, that's say my father",
+    "important": true,
+    "isDone": false,
+    "modified": "2023-08-30T18:36:45.742Z"
+  },
+  {
+    "id": "7306dcb4-6150-4dce-856b-1dddf022948e",
+    "title": "Browsers",
+    "content": "Browser can execute only JavaScript",
+    "createdAt": "2023-08-26T20:06:26.227Z",
+    "modified": "2023-08-30T02:55:09.638Z",
+    "important": false,
+    "isDone": false
+  },
+  {
+    "id": "a5d1bfe9-3b32-465f-8a2a-876256d71a4c",
+    "title": "GET & POST",
+    "content": "GET and POST are the most important methods of HTTP protocol",
+    "important": true,
+    "isDone": false,
+    "modified": "2023-08-30T05:42:06.047Z"
+  },
+  {
+    "id": "8a0e67f1-02ca-4c1c-94b6-a4430924ab63",
+    "title": "Notes App",
+    "content": "Bienvenido a la app de notas en desarrollo, una aplicación que te permite crear, editar y organizar tus notas de forma fácil y rápida. Con esta app, puedes guardar tus ideas, recordatorios, listas, proyectos y más en un solo lugar. También puedes sincronizar tus notas con otros dispositivos y acceder a ellas desde cualquier lugar. Quedó funcionando bien por dicha",
+    "createdAt": "2023-08-30T02:55:22.939Z",
+    "modified": "2023-08-30T02:55:22.939Z",
+    "important": false,
+    "isDone": false
+  },
+  {
+    "id": "b85fab9e-955a-46ab-8bbb-4b10a2e91504",
+    "title": "Notes App",
+    "content": "Bienvenido a la app de notas en desarrollo, una aplicación que te permite crear, editar y organizar tus notas de forma fácil y rápida. Con esta app, puedes guardar tus ideas, recordatorios, listas, proyectos y más en un solo lugar. También puedes sincronizar tus notas con otros dispositivos y acceder a ellas desde cualquier lugar. Bienvenido a la app de notas en desarrollo, una aplicación que te permite crear, editar y organizar tus notas de forma fácil y rápida. Con esta app, puedes guardar tus ideas, recordatorios, listas, proyectos y más en un solo lugar. También puedes sincronizar tus notas con otros dispositivos y acceder a ellas desde cualquier lugar. Bienvenido a la app de notas en desarrollo, una aplicación que te permite crear, editar y organizar tus notas de forma fácil y rápida. Con esta app, puedes guardar tus ideas, recordatorios, listas, proyectos y más en un solo lugar. También puedes sincronizar tus notas con otros dispositivos y acceder a ellas desde cualquier lugar. Bienvenido a la app de notas en desarrollo, una aplicación que te permite crear, editar y organizar tus notas de forma fácil y rápida. Con esta app, puedes guardar tus ideas, recordatorios, listas, proyectos y más en un solo lugar. También puedes sincronizar tus notas con otros dispositivos y acceder a ellas desde cualquier lugar. Bienvenido a la app de notas en desarrollo, una aplicación que te permite crear, editar y organizar tus notas de forma fácil y rápida. Con esta app, puedes guardar tus ideas, recordatorios, listas, proyectos y más en un solo lugar. También puedes sincronizar tus notas con otros dispositivos y acceder a ellas desde cualquier lugar.",
+    "important": true,
+    "isDone": false,
+    "modified": "2023-08-30T05:41:48.447Z"
+  },
+  {
+    "id": "881f6de0-b5a6-48f6-83a2-0ed88526a532",
+    "title": "sd",
+    "content": "Sin contenido",
+    "createdAt": "2023-08-31T17:33:39.399Z",
+    "modified": "2023-08-31T17:33:39.399Z",
+    "important": false,
+    "isDone": false
+  },
+  {
+    "id": "c47c7929-c9e0-4c02-bf4f-5096127dfb8e",
+    "title": "sd",
+    "content": "Sin contenido",
+    "createdAt": "2023-08-31T17:33:40.335Z",
+    "modified": "2023-08-31T17:33:40.335Z",
+    "important": false,
+    "isDone": false
+  },
+  {
+    "id": "92b7a614-c5e3-4dd7-acbe-5eb6681880fb",
+    "title": "sin titulo",
+    "content": "sd\n",
+    "createdAt": "2023-08-31T17:35:25.413Z",
+    "modified": "2023-08-31T17:35:25.413Z",
+    "important": false,
+    "isDone": false
+  }
+];
